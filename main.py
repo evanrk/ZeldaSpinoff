57c6e8fec249c5a368636f894e6aea93dc7df6b0
Evan Klein <evanrklein@gmail.com>
2023-02-22 13:19:25 -0500

    updated map class

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import pygame
 
-import map
+from map import Map
 import block
 
 BACKGROUND = (150, 150, 150)
 
 # set dimensions of the screen
-screen = pygame.display.set_mode((800, 600))
+SCREEN_SIZE = (800, 600)
+screen = pygame.display.set_mode(SCREEN_SIZE)
 
 # caption
 pygame.display.set_caption("Zelda?")
@@ -17,14 +18,15 @@ pygame.display.flip()
 
 
 # x:y = 4:3
-map = map.Map([
-[1, 1, 1, 1, 1, 1, 1, 1]
-[1, 1, 1, 1, 1, 1, 1, 1]
-[1, 1, 1, 1, 1, 1, 1, 1]
-[1, 1, 1, 1, 1, 1, 1, 1]
+map = Map(SCREEN_SIZE, [
+[1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1],
+[1, 1, 1, 1, 1, 1, 1, 1],
 [1, 1, 1, 1, 1, 1, 1, 1]
 ])
 
+print(map)
 
 # run = True
 # while run:
